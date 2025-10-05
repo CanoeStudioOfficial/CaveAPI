c8a2d0138eba3a0ec22258ebbc4d0677dc2685ab
Lonelyxiya <3438505487@qq.com>
2025-10-05 20:42:13 +0800

    Update Utils.java

--- src/main/java/caveapi/utils/Utils.java
+++ src/main/java/caveapi/utils/Utils.java
@@ -5,26 +5,25 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.chunk.ChunkPrimer;
 
-public class Utils {
+public class Utils
+{
     private static final int MAX_Y = 60;
 
-    public static int findFloor(ChunkPrimer primer, int x, int z) {
-        for(int y = 10; y < 60; ++y) {
+    public static int findFloor(final ChunkPrimer primer, final int x, final int z) {
+        for (int y = 10; y < 60; ++y) {
             if (primer.getBlockState(x, y, z).getBlock() == Blocks.AIR && primer.getBlockState(x, y - 1, z).isOpaqueCube()) {
                 return y;
             }
         }
-
         return -1;
     }
 
-    public static int findCeiling(ChunkPrimer primer, int x, int z, int fromY) {
-        for(int y = fromY + 2; y < 80; ++y) {
+    public static int findCeiling(final ChunkPrimer primer, final int x, final int z, final int fromY) {
+        for (int y = fromY + 2; y < 80; ++y) {
             if (!primer.getBlockState(x, y, z).getBlock().isAir(primer.getBlockState(x, y, z), (IBlockAccess)null, (BlockPos)null)) {
                 return y;
             }
         }
-
         return -1;
     }
 }
